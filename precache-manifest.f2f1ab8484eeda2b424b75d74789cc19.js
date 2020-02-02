3736285daed1df4d7582db43fe25bdd2240dfc0b
ashishkumar47 <ashishkumar8163022@gmail.com>
2020-02-02 12:34:06 +0545

    Updates

---
+++ precache-manifest.f2f1ab8484eeda2b424b75d74789cc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0afdcac8fb569ed8840a7432686c2f",
+    "url": "/Pokedexgame/index.html"
+  },
+  {
+    "revision": "97807c5ea9fa39d3e59e",
+    "url": "/Pokedexgame/static/css/main.518a8e74.chunk.css"
+  },
+  {
+    "revision": "d0d1b443dc542ffcb5bf",
+    "url": "/Pokedexgame/static/js/2.39ded7d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Pokedexgame/static/js/2.39ded7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97807c5ea9fa39d3e59e",
+    "url": "/Pokedexgame/static/js/main.016442a3.chunk.js"
+  },
+  {
+    "revision": "6dbcca983051ae2e008f",
+    "url": "/Pokedexgame/static/js/runtime-main.aec7c7c6.js"
+  }
+]);
